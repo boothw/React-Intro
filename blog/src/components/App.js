d324b59a3e40661ffc84cb23b73f3d4653d8358c
Will Booth <willbooth97@hotmail.co.uk>
2020-08-05 15:29:51 +0100

    Learning how to Thunk

---
+++ blog/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="ui container">
+            <div className="ui container grid">
+                <div className="ui row">
+                    App
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
